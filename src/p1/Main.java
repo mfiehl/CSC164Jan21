385aa54c2e4a4003bd4957dccd31264e9fae32b4
mfiehl <mfiehl@dtcc.edu>
2016-01-21 14:30:06 -0500

    Updated

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,9 +1,48 @@
+
 package p1;
 
-public class Main {
+/*
+ * Program for an ordering system
+ * CSC 164
+ * Mike Fiehl
+ * 21, Jan 2016
+ * Version 0.1
+ */
 
+import java.util.Scanner;
+
+public class Main
+{
     public static void main(String[] args)
     {
+        //Allow user input
+        Scanner input = new Scanner(System.in);
 
+        //Declare variables
+        char option;
+
+        System.out.println("Welcome to Jimmy Johns");
+
+        do
+        {
+            System.out.println("(H)am");
+            System.out.println("(T)urkey");
+            System.out.println("(S)alami");
+            System.out.println("(R)oast Beef");
+            System.out.println("(D)one");
+            System.out.print("Enter your choice: ");
+
+            //Get user input and convert to upper case
+            String choice = input.next();
+            choice = choice.toUpperCase();
+
+            //Get first character of user input
+            option = choice.charAt(0);
+
+            System.out.println("choice = " + choice + ", option = " + option);
+
+        }while(option != 'D'); //End do-while
+
+        System.out.println("Thank you for ordering at Jimmy Johns");
     }
 }
